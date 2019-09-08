d547110c50fb55d45cefa3d91e066ce4f12f77ea
Wolfgang Meier <wolfgang@existsolutions.com>
2019-09-08 19:04:20 +0200

    Port pb-progress

---
+++ pb-components.js
@@ -0,0 +1,15 @@
+import './src/pb-browse-docs.js';
+import './src/pb-collapse.js';
+import './src/pb-document.js';
+import './src/pb-highlight.js';
+import './src/pb-link.js';
+import './src/pb-load.js';
+import './src/pb-mixin.js';
+import './src/pb-navigation.js';
+import './src/pb-page.js';
+import './src/pb-paginate.js';
+import './src/pb-progress.js';
+import './src/pb-search.js';
+import './src/pb-toggle-feature.js';
+import './src/pb-view.js';
+import './src/pb-zoom.js';
---
+++ src/pb-progress.js
@@ -0,0 +1,62 @@
+import { LitElement, html, css } from 'lit-element';
+import { pbMixin } from './pb-mixin.js';
+
+
+/**
+ * A progress bar which becomes active when signal `pb-start-update` is
+ * being sent and is deactivated on `pb-end-update`.
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/pb-progress.html
+ * @appliesMixin pbMixin
+ */
+export class PbProgress extends pbMixin(LitElement) {
+    static get properties() {
+        return {
+            ...super.properties
+        };
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+
+        this.subscribeTo('pb-start-update', this._startUpdate.bind(this));
+        this.subscribeTo('pb-end-update', this._endUpdate.bind(this));
+    }
+
+    firstUpdated() {
+        this.shadowRoot.getElementById('progress').disabled = true;
+    }
+
+    render() {
+        return html`
+            <paper-progress id="progress" indeterminate></paper-progress>
+        `;
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+                visibility: hidden;
+            }
+
+            paper-progress {
+                width: 100%;
+            }
+        `;
+    }
+
+    _startUpdate() {
+        this.style.visibility = 'visible';
+        this.$.progress.disabled = false;
+        console.log('<pb-progress> start update');
+    }
+
+    _endUpdate() {
+        this.style.visibility = 'hidden';
+        this.$.progress.disabled = true;
+    }
+}
+customElements.define('pb-progress', PbProgress);
