c1f12350b8a0f58fd802672a5d91bb11d7f2a682
Wolfgang Meier <wolfgang@exist-db.org>
2020-01-15 15:04:37 +0100

    Translate pb-edit-app

--- src/pb-edit-app.js
+++ src/pb-edit-app.js
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit-element';
 import { pbMixin } from './pb-mixin.js';
+import { translate } from './pb-i18n.js';
 import './pb-select-odd.js';
 import '@polymer/paper-input/paper-input.js';
 import '@polymer/paper-button';
@@ -100,67 +101,63 @@ export class PbEditApp extends pbMixin(LitElement) {
                 <form action="modules/components-generate.xql" method="POST" accept="application/json" enctype="application/json">
                     <pb-select-odd id="odd" name="odd" label="ODD" odd="teipublisher"></pb-select-odd>
                     <paper-input name="uri" type="url" required placeholder="http://exist-db.org/apps/my-simple-app"
-                        label="URL to uniquely identify the app" auto-validate></paper-input>
-                    <paper-input id="abbrev" name="abbrev" pattern="[a-zA-Z0-9-_]+" required placeholder="Short name"
-                        label="Short name to be used in URLs and file names" auto-validate></paper-input>
+                        label="${translate('appgen.uri')}" auto-validate></paper-input>
+                    <paper-input id="abbrev" name="abbrev" pattern="[a-zA-Z0-9-_]+" required placeholder="${translate('appgen.abbrev.placeholder')}"
+                        label="${translate('appgen.abbrev.label')}" auto-validate></paper-input>
                     <paper-input name="data-collection" pattern="[a-zA-Z0-9-_/]+" placeholder="data"
-                        label="Name of subcollection to hold TEI documents" auto-validate></paper-input>
-                    <paper-input name="title" required placeholder="Title of the app"
-                        label="Title of the app which will be shown e.g. in the dashboard"></paper-input>
+                        label="${translate('appgen.collection')}" auto-validate></paper-input>
+                    <paper-input name="title" required placeholder="${translate('appgen.title.label')}"
+                        label="${translate('appgen.title.help')}"></paper-input>
                     <fieldset>
-                        <legend>Choose the HTML template to be used as default</legend>
-                        <paper-dropdown-menu id="template" label="HTML Template" name="template">
+                        <legend>${translate('appgen.template.help')}</legend>
+                        <paper-dropdown-menu id="template" label="${translate('appgen.template.label')}" name="template">
                             <paper-listbox slot="dropdown-content" class="dropdown-content" attr-for-selected="value" selected="view.html">
                             ${this.templates.map(t => html`<paper-item value="${t.name}">${t.title}</paper-item>`)}
                             </paper-listbox>
                         </paper-dropdown-menu>
                     </fieldset>
                     <fieldset>
-                        <legend>Choose what is shown by default when browsing text: a single page
-                        or an entire division. Display by page requires that the TEI is properly marked up
-                        with &lt;tei:pb&gt; tags.</legend>
-                        <paper-dropdown-menu id="defaultView" label="Default View" name="default-view">
+                        <legend>${translate('appgen.view.help')}</legend>
+                        <paper-dropdown-menu id="defaultView" label="${translate('appgen.label')}" name="default-view">
                             <paper-listbox slot="dropdown-content" class="dropdown-content" selected="div" attr-for-selected="value">
-                                <paper-item value="div">By division (chapter/section...)</paper-item>
-                                <paper-item value="page">By page</paper-item>
+                                <paper-item value="div">${translate('appgen.view.div')}</paper-item>
+                                <paper-item value="page">${translate('appgen.view.page')}</paper-item>
                             </paper-listbox>
                         </paper-dropdown-menu>
                     </fieldset>
                     <fieldset>
-                        <legend>Define the smallest block on which a full text index is created. For documents
-                        organized into divisions, choosing 'Create on division' is usually best. If there are no divisions,
-                        choose 'Create on text'.</legend>
-                        <paper-dropdown-menu id="index" label="Default Full Text Index" name="index">
+                        <legend>${translate('appgen.index.help')}</legend>
+                        <paper-dropdown-menu id="index" label="${translate('appgen.index.label')}" name="index">
                             <paper-listbox slot="dropdown-content" class="dropdown-content" selected="tei:div" attr-for-selected="value">
-                                <paper-item value="tei:div">Create on division</paper-item>
-                                <paper-item value="tei:text">Create on text</paper-item>
+                                <paper-item value="tei:div">${translate('appgen.index.index-div')}</paper-item>
+                                <paper-item value="tei:text">${translate('appgen.index.index-text')}</paper-item>
                             </paper-listbox>
                         </paper-dropdown-menu>
                     </fieldset>
                     <fieldset>
-                        <legend>User account for administrative tasks. The user will be created if it does not yet exist.</legend>
-                        <paper-input name="owner" required placeholder="Username"
-                            label="The user account who will own the app." auto-validate></paper-input>
-                        <paper-input name="password" type="password" required placeholder="Password"
-                            label="Password for the user owning this app." auto-validate></paper-input>
+                        <legend>${translate('appgen.account.user')}</legend>
+                        <paper-input name="owner" required placeholder="${translate('login.user')}"
+                            label="${translate('appgen.account.owner')}" auto-validate></paper-input>
+                        <paper-input name="password" type="password" required placeholder="${translate('login.password')}"
+                            label="${translate('appgen.account.password')}" auto-validate></paper-input>
                     </fieldset>
-                    <paper-button id="submit" @click="${this._doSubmit}"><iron-icon icon="save"></iron-icon> Save/Generate</paper-button>
+                    <paper-button id="submit" @click="${this._doSubmit}"><iron-icon icon="save"></iron-icon> ${translate('appgen.submit')}</paper-button>
                 </form>
             </iron-form>
             <paper-dialog id="dialog">
-                <h2>App generated</h2>
+                <h2>${translate('appgen.dialog.title')}</h2>
                 <div id="dialogContent">
                 ${
                     this.error ?
                         html`<a href="${this.url}" target="_blank">
-                            <paper-button><iron-icon icon="icons:open-in-new"></iron-icon> Open</paper-button>
+                            <paper-button><iron-icon icon="icons:open-in-new"></iron-icon> ${translate('appgen.open')}</paper-button>
                         </a>
-                        <p>Your app has been successfully generated!</p>` :
+                        <p>${translate('appgen.success')}</p>` :
                         html`<div id="error">${this.error}</div>`
                 }
                 </div>
                 <div class="buttons">
-                    <paper-button dialog-dismiss autofocus>Close</paper-button>
+                    <paper-button dialog-dismiss autofocus>${translate('dialogs.close')}</paper-button>
                 </div>
             </paper-dialog>
 
