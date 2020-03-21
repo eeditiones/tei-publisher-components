bffe4915991de403faec7ea515b048fbbc4a03fb
Wolfgang Meier <wolfgang@existsolutions.com>
2020-03-21 17:25:56 +0100

    Also bundle pb-demo-snippet

--- src/pb-components-all.js
+++ src/pb-components-all.js
@@ -1,3 +1,4 @@
 import './pb-components-bundle.js';
 import './pb-facsimile.js';
 import './pb-leaflet-map.js';
+import './pb-odd-editor.js';
--- src/pb-components-bundle.js
+++ src/pb-components-bundle.js
@@ -1,4 +1,5 @@
 import '@webcomponents/webcomponentsjs/webcomponents-loader.js';
 import '@polymer/paper-fab';
 import '@polymer/paper-card';
+import './docs/pb-demo-snippet.js';
 import './pb-components.js';
