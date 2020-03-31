7885235be989e5ba92e2074a33c0ca8bf030b118
Wolfgang Meier <wolfgang@exist-db.org>
2020-03-31 10:15:17 +0200

    Increment version

--- es-dev-server.config.js
+++ es-dev-server.config.js
@@ -2,11 +2,5 @@ module.exports = {
     watch: true,
     nodeResolve: true,
     appIndex: 'docs.html',
-    open: true,
-    responseTransformers: [
-        function rewriteBasePath({ url, status, contentType, body }) {
-            const rewritten = body.replace(/process.env.NODE_ENV/g, JSON.stringify('production'));
-            return { body: rewritten };
-        },
-    ]
+    open: true
 };
