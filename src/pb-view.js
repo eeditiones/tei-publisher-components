a1464fa467a44cb3c0d284c8fb1c7c813fa05897
Wolfgang Meier <wolfgang@existsolutions.com>
2020-04-05 20:05:30 +0200

    Do not import pb-popover into pb-view to avoid bundling issues

--- src/pb-view.js
+++ src/pb-view.js
@@ -5,8 +5,6 @@ import { translate } from "./pb-i18n.js";
 import '@polymer/iron-ajax';
 import '@polymer/paper-dialog';
 import '@polymer/paper-dialog-scrollable';
-import './pb-highlight.js';
-import './pb-popover.js';
 
 /**
  * This is the main component for viewing text which has been transformed via an ODD.
@@ -447,11 +445,6 @@ export class PbView extends pbMixin(LitElement) {
         }
         this.xmlId = null;
 
-        // const popovers = elem.querySelector('pb-popover');
-        // if (popovers) {
-        //     // eslint-disable-next-line babel/no-unused-expressions
-        //     import('./pb-popover.js');
-        // }
         this._fixLinks(elem);
         this._applyToggles(elem);
 
