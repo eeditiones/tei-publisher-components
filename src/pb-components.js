733d29c66ddd2f9112fb7f0e171fb4eb947e50d1
Wolfgang Meier <wolfgang@existsolutions.com>
2020-04-13 19:25:45 +0200

    pb-code-editor should be bundled into pb-components-bundle

--- src/pb-components.js
+++ src/pb-components.js
@@ -35,6 +35,7 @@ import './pb-select-odd.js';
 import './pb-select-template.js';
 import './pb-geolocation.js';
 import './pb-code-highlight.js';
+import './pb-code-editor.js';
 import './pb-repeat.js';
 import './pb-svg.js';
 import './pb-facsimile.js';
