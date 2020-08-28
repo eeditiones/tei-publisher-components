01f84fbd5c88954bfa550bbe1a2f0a1d52c48138
Magdalena Turska <tuurma@gmail.com>
2020-08-28 20:07:11 +0200

    fix(tests) adjust pb-autocomplete and pb-select tests

--- test/pb-autocomplete.test.js
+++ test/pb-autocomplete.test.js
@@ -2,11 +2,8 @@ import { fixture, expect, waitUntil } from '@open-wc/testing';
 import '../src/pb-autocomplete.js';
 import '../src/pb-page.js';
 import '@polymer/paper-item';
+import '@polymer/iron-form';
 
-
-function serializeForm(form) {
-    return new URLSearchParams(new FormData(form)).toString();
-}
 
 describe('simple autocomplete with static suggestions', () => {
     it('submits in form', async () => {
@@ -17,17 +14,19 @@ describe('simple autocomplete with static suggestions', () => {
         const el = (
             await fixture(`
                 <pb-page endpoint=".">
-                    <form action="">
-                        <pb-autocomplete name="lang" placeholder="Language" value="pl" icon="icons:flag"
-                            suggestions='[{"text": "English", "value": "en"}, {"text": "Polish", "value": "pl"}, {"text": "German", "value": "de"}]'>
-                        </pb-autocomplete>
-                    </form>
+                    <iron-form id="form">
+                        <form action="">
+                            <pb-autocomplete name="lang" placeholder="Language" value="pl" icon="icons:flag"
+                                suggestions='[{"text": "English", "value": "en"}, {"text": "Polish", "value": "pl"}, {"text": "German", "value": "de"}]'>
+                            </pb-autocomplete>
+                        </form>
+                    </iron-form>
                 </pb-page>
             `)
         );
         await waitUntil(() => initDone);
 
-        const form = el.querySelector('form');
-        expect(serializeForm(form)).to.equal('lang=pl');
+        const form = el.querySelector('iron-form');
+        expect(form.serializeForm()).to.deep.equal({'lang': 'pl'});
     });
 });
