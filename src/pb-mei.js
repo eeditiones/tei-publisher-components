5de4d03fc63d48b3be0fff72ae548f38b8fc96f7
Wolfgang Meier <wolfgang@existsolutions.com>
2021-04-08 11:24:50 +0200

    feat(pb-mei): support dynamic switching of app/lem/rdg and other alternates in MEI content

--- src/pb-mei.js
+++ src/pb-mei.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit-element';
+import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
 import "@lrnwebcomponents/es-global-bridge";
 import { pbMixin } from './pb-mixin.js';
 import { translate } from './pb-i18n.js';
@@ -24,46 +25,76 @@ function _import(name, location) {
  * Supports optional MIDI playback using [web-midi-player](https://midi.yvesgurcan.com/).
  * Both libraries are loaded dynamically when the component is used the first time.
  * 
- * @prop {"auto" | "encoded" | "none"} footer - Control footer layout
+ * @prop {"auto" | "encoded" | "none" | "always"} footer - Control footer layout
  * @prop {"auto" | "encoded" | "none"} header - Control footer layout
- * @prop {"auto" | "none" | "line" | "encoded"} Define page and system breaks layout (default: "auto")
+ * @prop {"auto" | "none" | "line" | "encoded" | "smart"} breaks - Define page and system breaks layout (default: "auto")
  */
 export class PbMei extends pbMixin(LitElement) {
 
     static get properties() {
         return {
-            /**
-             * URL of the MEI file to load. A relative URL would be resolved
-             * relative to the TEI Publisher endpoint.
-             */
-            url: {
-                type: String
-            },
-            /**
-             * Set to enable MIDI player
-             */
-            player: {
-                type: Boolean
-            },
-            header: {
-                type: String
-            },
-            footer: {
-                type: String
-            },
-            breaks: {
-                type: String
-            },
-            _pages: {
-                type: Number
-            },
-            ...super.properties
+          /**
+           * URL of the MEI file to load. A relative URL would be resolved
+           * relative to the TEI Publisher endpoint.
+           */
+          url: {
+            type: String,
+          },
+          /**
+           * Set to enable MIDI player
+           */
+          player: {
+            type: Boolean,
+          },
+          header: {
+            type: String,
+          },
+          footer: {
+            type: String,
+          },
+          breaks: {
+            type: String,
+          },
+          /**
+           * Set the XPath query for selecting <app> child elements, for example: "./rdg[contains(@source, 'source-id')]";
+           * by default the <lem> or the first <rdg> is selected
+           */
+          appXPath: {
+            type: String,
+            attribute: 'app-xpath',
+          },
+          /**
+           * Set the xPath query for selecting <choice> child elements, for example: "./orig"; by default the first child is selected
+           */
+          choiceXPath: {
+            type: String,
+            attribute: 'choice-xpath',
+          },
+          /**
+           * Set the xPath query for selecting the <mdiv> to be rendered; only one <mdiv> can be rendered (default: "")
+           */
+          mdivXPath: {
+            type: String,
+            attribute: 'mdiv-xpath',
+          },
+          /**
+           * Set the xPath query for selecting <subst> child elements, for example: "./del"; by default the first child is selected
+           */
+          substXPath: {
+            type: String,
+            attribute: 'subst-xpath',
+          },
+          _svg: {
+            type: String,
+          },
+          ...super.properties,
         };
     }
 
     constructor() {
         super();
         this._data = null;
+        this._svg = null;
         this._pages = 0;
         this._page = 0;
         this._midiPlayer = null;
@@ -71,6 +102,10 @@ export class PbMei extends pbMixin(LitElement) {
         this.footer = 'auto';
         this.header = 'auto';
         this.breaks = 'auto';
+        this.appXPath = null;
+        this.choiceXPath = null;
+        this.mdivXPath = null;
+        this.substXPath = null;
     }
 
     firstUpdated() {
@@ -89,29 +124,46 @@ export class PbMei extends pbMixin(LitElement) {
                 this._verovio = new window.verovio.toolkit();
 
                 PbMei.waitOnce('pb-page-ready', () => {
-                    this.url = this.toAbsoluteURL(this.url);
-
-                    fetch(this.url)
-                        .then(response => response.text())
-                        .then(async data => {
-                            this._data = data;
-                            this._verovio.loadData(this._data);
-                            this._pages = this._verovio.getPageCount();
-                            this._page = 1;
-                            console.log('<pb-mei> Loaded %d pages', this._pages);
-                            this.showPage();
-                        });
+                    this.load();
                 });
             });
     }
 
-    showPage() {
-        this._verovio.setOptions(this._getOptions());
-        const svg = this._verovio.renderToSVG(this._page, {});
-        this.shadowRoot.getElementById('output').innerHTML = svg;
+    update(changedProps) {
+        super.update(changedProps);
+        if (changedProps.get('url')) {
+            this.load();
+        } else if (
+          (changedProps.has('appXPath') ||
+            changedProps.has('choiceXPath') ||
+            changedProps.has('mdivXPath') ||
+            changedProps.has('substXPath')) &&
+          this._verovio
+        ) {
+          this._verovio.setOptions(this._getOptions());
+          this._verovio.loadData(this._data);
+          this.showPage();
+        }
+    }
 
-        this.shadowRoot.getElementById('pageRight').disabled = this._page === this._pages;
-        this.shadowRoot.getElementById('pageLeft').disabled = this._page === 1;
+    load() {
+        const link = this.toAbsoluteURL(this.url);
+
+        fetch(link)
+          .then(response => response.text())
+          .then(async data => {
+            this._data = data;
+            this._verovio.setOptions(this._getOptions());
+            this._verovio.loadData(this._data);
+            this._pages = this._verovio.getPageCount();
+            this._page = 1;
+            console.log('<pb-mei> Loaded %d pages', this._pages);
+            this.showPage();
+          });
+    }
+
+    showPage() {
+        this._svg = this._verovio.renderToSVG(this._page, {});
     }
 
     async play() {
@@ -154,12 +206,18 @@ export class PbMei extends pbMixin(LitElement) {
         return html`
             <div id="toolbar">
                 <paper-icon-button id="pageLeft" icon="icons:chevron-left" @click="${this._previousPage}"
+                    ?disabled="${this._page === 1}"
                     class="${this._pages === 1 ? 'hidden' : ''}"></paper-icon-button>
                 <paper-icon-button id="pageRight" icon="icons:chevron-right" @click="${this._nextPage}"
+                    ?disabled="${this._page === this._pages}"
                     class="${this._pages === 1 ? 'hidden' : ''}"></paper-icon-button>
                 ${ this._renderPlayer() }
+                <slot></slot>
             </div>
-            <div id="output">${translate('dialogs.loading')}</div>
+            ${ this._svg ? 
+                html`<div id="output">${unsafeHTML(this._svg)}</div>` :
+                html`<div id="output">${translate('dialogs.loading')}</div>`
+            }
         `;
     }
 
@@ -193,13 +251,16 @@ export class PbMei extends pbMixin(LitElement) {
 
     _getOptions() {
         const options = {
-            // svg content should scale with the width of the component
-            svgViewBox: true,
-            adjustPageHeight: true,
-            adjustPageWidth: true,
-            footer: this.footer,
-            header: this.header,
-            breaks: this.breaks
+          // svg content should scale with the width of the component
+          svgViewBox: true,
+          adjustPageHeight: true,
+          footer: this.footer,
+          header: this.header,
+          breaks: this.breaks,
+          appXPathQuery: [this.appXPath],
+          substXPathQuery: [this.substXPath],
+          mdivXPathQuery: this.mdivXPath,
+          choiceXPathQuery: [this.choiceXPath]
         };
         return options;
     }
@@ -213,6 +274,7 @@ export class PbMei extends pbMixin(LitElement) {
 
             #toolbar {
                 display: flex;
+                align-items: center;
             }
 
             #output {
