e99f61ead98eabf3faf2d884718efbf94b2aef2a
Wolfgang Meier <wolfgang@exist-db.org>
2021-05-15 16:40:23 +0200

    feat(pb-markdown): react to zoom-in/-out event

--- src/pb-markdown.js
+++ src/pb-markdown.js
@@ -48,19 +48,19 @@ export class PbMarkdown extends pbMixin(LitElement) {
         return {
             /**
              * The markdown content to be rendered. If undefined,
-             * markdown will be taken from the content of the element 
+             * markdown will be taken from the content of the element
              * or loaded from the specified URL.
              */
             content: {
-                type: String
+                type: String,
             },
             /**
              * An absolute or relative URL to load the markdown from.
              */
             url: {
-                type: String
+                type: String,
             },
-            ...super.properties
+            ...super.properties,
         };
     }
 
@@ -71,36 +71,45 @@ export class PbMarkdown extends pbMixin(LitElement) {
 
     set url(value) {
         this._url = value;
-        PbMarkdown.waitOnce('pb-page-ready', (options) => {
+        PbMarkdown.waitOnce("pb-page-ready", (options) => {
             this._load(options.endpoint);
         });
     }
 
     connectedCallback() {
         super.connectedCallback();
-        this.style.display = 'block';
+        this.style.display = "block";
 
         if (!this.content) {
-            const content = document.createElement('div');
+            const content = document.createElement("div");
             for (let i = 0; i < this.childNodes.length; i++) {
                 const node = this.childNodes[i];
-                content.appendChild(document.importNode(node.content || node, true));
+                content.appendChild(
+                    document.importNode(node.content || node, true)
+                );
             }
             this.content = removeIndent(content.innerHTML);
         }
+
+        this.subscribeTo("pb-zoom", (ev) => {
+            this.zoom(ev.detail.direction);
+        });
     }
 
     _load(server) {
         const url = this.toAbsoluteURL(this._url, server);
-        fetch(url, { credentials: 'same-origin' })
-          .then(response => response.text())
-          .catch(() => {
-            console.error('<pb-markdown> failed to load content from %s', url.toString());
-            return Promise.resolve(this.content);
-          })
-          .then(text => {
-            this.content = text;
-          });
+        fetch(url, { credentials: "same-origin" })
+            .then((response) => response.text())
+            .catch(() => {
+                console.error(
+                    "<pb-markdown> failed to load content from %s",
+                    url.toString()
+                );
+                return Promise.resolve(this.content);
+            })
+            .then((text) => {
+                this.content = text;
+            });
     }
 
     createRenderRoot() {
@@ -112,6 +121,19 @@ export class PbMarkdown extends pbMixin(LitElement) {
             return null;
         }
         return html`<div>${unsafeHTML(window.marked(this.content))}</div>`;
+    }
+
+    zoom(direction) {
+        const fontSize = window
+            .getComputedStyle(this)
+            .getPropertyValue("font-size");
+        const size = parseInt(fontSize.replace(/^(\d+)px/, "$1"));
+
+        if (direction === "in") {
+            this.style.fontSize = size + 1 + "px";
+        } else {
+            this.style.fontSize = size - 1 + "px";
+        }
     }
 }
 customElements.define('pb-markdown', PbMarkdown);
