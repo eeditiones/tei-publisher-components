9a74fcddf96d6549820971d72d37a804038fc5f9
JoernT <joern.turner@gmail.com>
2021-06-17 14:50:25 +0200

    fixed pagination; refined layout;

--- src/pb-kwic-results.js
+++ src/pb-kwic-results.js
@@ -45,7 +45,7 @@ export class PbKwicResults extends pbMixin(LitElement) {
         // this.data = responseData;
         console.log('data ', this.data);
         this.data = {documents: []};
-        this.first = 0;
+        this.first = 1;
         this.path = '';
         this.doc = null;
     }
@@ -57,77 +57,83 @@ export class PbKwicResults extends pbMixin(LitElement) {
                 display: block;
                 width:100%;
             }
-            .docref{
-                font-weight:700;
+            table{
+                width:100%;
+                margin-top:1rem;
             }
-
-            .grid{
-                display:grid;
-                grid-template-columns:1fr min-content 1fr;
-                grid-gap:1.5rem;
+            th:nth-child(1),td:nth-child(1){
+                width:35%;
             }
-            .grid:nth-child(1),
-            .grid .left{
-                justify-self:end;
+            th:nth-child(5), td:nth-child(5){
+                width:35%;
             }
-
-            section .grid .left:before{
-                content:'...';
+            .left, .hit-count{
+                text-align:right;
             }
-            .grid .hit{
-                justify-self:center;
+            .right{
+                text-align:left;
+            }
+            td.hit{
+                text-align:center;
                 white-space:nowrap;
-                font-weight:700;
             }
-
-            section{
-                    padding:1rem;
-            }
-            section .grid .right:after{
-                content:'...';
-            }
-
         `;
     }
 
     connectedCallback() {
         super.connectedCallback();
         PbKwicResults.waitOnce('pb-page-ready', () => {
-
-            // this.url = `${this.getEndpoint()}/api/blacklab/search?pattern=${this.pattern}&start=${this.start}&per-page=${this.perPage}`;
-            // console.log('endpoint ', url);
+            console.log('page-ready');
             this.load();
-
         });
-
 
         this.subscribeTo('pb-load', (event) => {
-            this.first = event.detail.params["start"];
-            this.perPage = event.detail.params["per-page"];
+            const index = Number(event.detail.params.page) + 1;
+            const perPage = Number(event.detail.params["per-page"]);
+
+            if(index === 1){
+                this.first = 1;
+            }else{
+                this.first = (index-1) * perPage+1;
+            }
             this.load();
         });
+
+
     }
 
     render() {
         return html`
-            <header class="grid">
-                <div class="left">before</div>
-                <div class="hit">hit</div>
-                <div class="right">after</div>
-            </header>
             <pb-paginate per-page="${this.perPage}" start="${this.first}" @pb-load="${this._handlePagination}"></pb-paginate>
-            ${this.data.documents.map(document => html`
-                <section>
-                    <div class="docref"><a href="${this.getEndpoint()}/api/blacklab/view?pattern=${this.pattern}&doc=${document.id}">${document.id}</a></div>
-                    <div class="grid">
-                        ${document.matches.map(match => html`
-                            <div class="left">${match.left}</div>
-                            <div class="hit"><a href="${this.getEndpoint()}/api/blacklab/view?pattern=${this.pattern}&doc=${document.id}&match=${match.match.words[0]}&page=${match.page[0]}">${match.match.display}</a></div>
-                            <div class="right">${match.right}</div>
-                        `)}
-                    </div>
-                </section>
-            `)}
+            <table>
+                <tr>
+                    <th>Doc Id</th>
+                    <th class="left">before</th>
+                    <th>hit</th>
+                    <th class="right">after</th>
+                    <th class="hit-count">hits</th>
+                </tr>
+                ${this.data.documents.map(document => html`
+
+                    <tr>
+                        <td colspan="4">
+                            <a class="docName" href="${this.getEndpoint()}/api/blacklab/view?pattern=${this.pattern}&doc=${document.id}">${document.id}</a>
+                        </td>
+                        <td class="hit-count">
+                            <span class="hit-count">${document.hits}</span>
+                        </td>
+                    </tr>
+                    ${document.matches.map(match => html`
+                        <tr>
+                            <td class="left" colspan="2">${match.left}</td>
+                            <td class="hit">
+                                <a href="${this.getEndpoint()}/api/blacklab/view?pattern=${this.pattern}&doc=${document.id}&match=${match.match.words[0]}&page=${match.page[0]}">${match.match.display}</a>
+                            </td>
+                            <td class="right" colspan="2">${match.right}</td>
+                        </tr>
+                    `)}
+                `)}
+            </table>
         `;
     }
 
@@ -136,17 +142,16 @@ export class PbKwicResults extends pbMixin(LitElement) {
         console.log('pattern ', this.pattern);
         console.log('first ', this.first);
         console.log('per-page ', this.perPage);
-
-
     }
 
     // async _load(url){
     async load() {
+        console.log('endpoint ', this.getEndpoint());
+        if(!this.getEndpoint()) return;
         let url = `${this.getEndpoint()}/api/blacklab/search?pattern=${this.pattern}&start=${this.first}&per-page=${this.perPage}`;
         if (this.doc) {
             url = `${this.getEndpoint()}/api/blacklab/search?pattern=${this.pattern}&start=${this.first}&per-page=${this.perPage}&doc=${this.doc}`;
         }
-        // const url = `${this.getEndpoint()}/api/blacklab/search?pattern=${this.pattern}&start=${this.first}&per-page=${this.perPage}`;
         await fetch(url, {
             method: 'GET',
             mode: 'cors',
@@ -156,6 +161,7 @@ export class PbKwicResults extends pbMixin(LitElement) {
             .then((data) => {
                 console.log('response ', data);
                 this.data = data;
+                this.first = data.start;
                 this.emitTo('pb-results-received', {
                     "count": data.docs ? parseInt(data.docs, 10) : 0,
                     "start": data.start,
@@ -163,7 +169,7 @@ export class PbKwicResults extends pbMixin(LitElement) {
                 });
             })
             .catch((error) => {
-                console.error('Error retrieving remote content: %o', error);
+                console.error('Error retrieving remote content: ', error);
             });
 
     }
