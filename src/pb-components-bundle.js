4ff3077241157390ca58833aec470517d668f575
JoernT <joern.turner@gmail.com>
2021-06-28 18:17:49 +0200

    fixed bundling

--- src/pb-components-bundle.js
+++ src/pb-components-bundle.js
@@ -1,10 +1,10 @@
+import '@jinntec/fore/dist/fore.js';
 import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
 import '@polymer/app-layout/app-scroll-effects/effects/material.js';
 import '@polymer/paper-tooltip/paper-tooltip.js';
 import '@polymer/paper-fab';
 import '@polymer/paper-card';
 import './docs/pb-demo-snippet.js';
-import '@jinntec/fore/dist/fore.js';
 
 
 import './pb-components.js';
