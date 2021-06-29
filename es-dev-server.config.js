adf321b1dcb02750514eb9ec1fddd282a1b9025b
Wolfgang Meier <wolfgang@exist-db.org>
2021-06-29 16:40:17 +0200

    build: add CORS header by default for es-dev-server

--- es-dev-server.config.js
+++ es-dev-server.config.js
@@ -4,12 +4,14 @@ module.exports = {
     appIndex: 'api.html',
     open: true,
     cors: true,
-    responseTransformers: [
-        function rewriteBasePath({ url, status, contentType, body }) {
-            if (body) {
-                const rewritten = body.replace(/process.env.NODE_ENV/g, JSON.stringify('production'));
-                return { body: rewritten };
+    plugins: [
+        {
+            transform(context) {
+                context.response.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+                return {
+                    body: context.body.replace(/process.env.NODE_ENV/g, JSON.stringify('production'))
+                };
             }
-        },
+        }
     ]
 };
