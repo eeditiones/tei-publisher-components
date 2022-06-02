024759ef177332e8e91a158871ccccbe09661e9b
Wolfgang Meier <wolfgang@existsolutions.com>
2022-06-02 11:39:04 +0200

    pb-multi-select: rename to pb-combo-box

--- src/pb-combo-box.js
+++ src/pb-combo-box.js
@@ -1,5 +1,4 @@
-import { LitElement, css, html } from 'lit-element';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html';
+import { LitElement } from 'lit-element';
 import TomSelect from "tom-select";
 import { pbMixin } from './pb-mixin.js';
 import { resolveURL } from './utils.js';
@@ -20,40 +19,83 @@ const renderDefault = (data) =>
     data ? `<div>${data.text}</div>` : '';
 
 /**
- *
- *
- * @customElement  pb-multi-select
- * @polymer
- * @demo demo/pb-multi-select.html
- * @appliesMixin pbMixin
+ * Provides a combo box, i.e. a combination of an input with a dropdown.
+ * Items to select from may be obtained from a remote data source.
+ * 
+ * The form control to be used (either a select or input) should be passed 
+ * in the content of the element. If no control is provided, a simple `<input>`
+ * will be created.
+ * 
  */
-export class PbMultiSelect extends pbMixin(LitElement) {
+export class PbComboBox extends pbMixin(LitElement) {
     static get properties() {
         return {
             ...super.properties,
-            theme: {
-                type: String
-            },
+            /**
+             * A remote data source to use. The component will pass the text entered by the user
+             * in parameter `query`. It expects a JSON array of objects with each object describing
+             * one item:
+             * 
+             * ```json
+             * {
+             *  "text": "Text to show as label",
+             *  "value": "value-to-use"
+             * }
+             * ```
+             */
             source: {
                 type: String
             },
+            /**
+             * Automatically close the dropdown once the user made a selection
+             */
             closeAfterSelect: {
                 type: Boolean,
                 attribute: 'close-after-select'
             },
+            /**
+             * Preload all items from the remote data source at startup
+             */
             preload: {
                 type: Boolean
+            },
+            /**
+             * Name of the event to be emitted when the user leaves the form control
+             */
+            onBlur: {
+                type: String,
+                attribute: 'on-blur'
+            },
+            /**
+             * Name of the event to be emitted when the value of the form control has changed
+             */
+            onChange: {
+                type: String,
+                attribute: 'on-change'
             }
         };
     }
 
+    /**
+     * Set a javascript function to be called whenever an item
+     * needs to be rendered. The function will be passed the data
+     * object of the current item as argument.
+     * 
+     * @param {Function} callbackFunction
+     */
+    set renderItem(callbackFunction) {
+        this.renderFunction = callbackFunction;
+    }
+
     constructor() {
         super();
-        this.theme = 'bootstrap5';
+        this.theme = 'default';
         this.source = null;
         this.closeAfterSelect = false;
         this.preload = false;
         this.renderFunction = renderDefault;
+        this.onBlur = 'pb-multi-select-blur';
+        this.onChange = 'pb-multi-select-change';
     }
 
     connectedCallback() {
@@ -72,7 +114,7 @@ export class PbMultiSelect extends pbMixin(LitElement) {
         }
         input.autocomplete = false;
 
-        PbMultiSelect.waitOnce('pb-page-ready', () => {
+        PbComboBox.waitOnce('pb-page-ready', () => {
             const options = {};
             if (this.source) {
                 const url = this.toAbsoluteURL(this.source);
@@ -99,22 +141,14 @@ export class PbMultiSelect extends pbMixin(LitElement) {
                 };
             }
             options.closeAfterSelect = this.closeAfterSelect;
-            options.onBlur = () => this.emitTo('pb-multi-select-blur');
-            options.onChange = () => this.emitTo('pb-multi-select-change');
+            options.onBlur = () => this.emitTo(this.onBlur);
+            options.onChange = () => this.emitTo(this.onChange);
             this._select = new TomSelect(input, options);
         });
-    }
-
-    static get styles() {
-        return css`
-            :host {
-                display: block;
-            }
-        `;
     }
 
     createRenderRoot() {
         return this;
     }
 }
-customElements.define('pb-multi-select', PbMultiSelect);
+customElements.define('pb-combo-box', PbComboBox);
