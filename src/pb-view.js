77851d2f038c97c1050b9dfa5fad79f517d37a02
Wolfgang Meier <wolfgang@existsolutions.com>
2022-08-04 16:56:50 +0200

    fix(pb-view): ignore external params if property is specified as attribute

--- src/pb-view.js
+++ src/pb-view.js
@@ -82,8 +82,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             * or the `.odd` suffix.
             */
             odd: {
-                type: String,
-                reflect: true
+                type: String
             },
             /**
             * The view type to use for paginating the document. Either `page`, `div` or `single`.
@@ -96,8 +95,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             * `single` | do not paginate but display entire content at once
             */
             view: {
-                type: String,
-                reflect: true
+                type: String
             },
             /**
             * An eXist nodeId. If specified, selects the root of the fragment of the document
@@ -105,7 +103,6 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             */
             nodeId: {
                 type: String,
-                reflect: true,
                 attribute: 'node-id'
             },
             /**
@@ -114,7 +111,6 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             */
             xmlId: {
                 type: Array,
-                reflect: true,
                 attribute: 'xml-id'
             },
             /**
@@ -126,8 +122,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             * If the `map` property is used, it may change scope for the displayed fragment.
             */
             xpath: {
-                type: String,
-                reflect: true
+                type: String
             },
             /**
             * If defined denotes the local name of an XQuery function in `modules/map.xql`, which will be called
@@ -187,8 +182,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
             */
             suppressHighlight: {
                 type: Boolean,
-                attribute: 'suppress-highlight',
-                reflect: true
+                attribute: 'suppress-highlight'
             },
             /**
             * CSS selector to find column breaks in the content returned
@@ -438,7 +432,10 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
         this.signalReady();
 
         if (this.onUpdate) {
-            this.subscribeTo('pb-update', this._refresh.bind(this));
+            this.subscribeTo('pb-update', () => {
+                this._setState(registry.getState(this));
+                this._refresh();
+            });
         }
     }
 
@@ -493,7 +490,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
                     this._features.language = data.language;
                 }
                 this.wait(() => {
-                    this._setState(registry.getState(this));
+                    this._setState(registry.state);
                     this._refresh();
                 });
             });
@@ -1174,10 +1171,10 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
                     break;
             }
         }
-        if (properties.odd) {
+        if (properties.odd && !this.getAttribute('odd')) {
             this.odd = properties.odd;
         }
-        if (properties.view) {
+        if (properties.view && !this.getAttribute('view')) {
             this.view = properties.view;
             if (this.view === 'single') {
                 // when switching to single view, clear current node id
@@ -1187,14 +1184,14 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
                 this.nodeId = this.switchView;
             }
         }
-        if (properties.xpath) {
+        if (properties.xpath && !this.getAttribute('xpath')) {
             this.xpath = properties.xpath;
         }
         if (properties.hasOwnProperty('columnSeparator')) {
             this.columnSeparator = properties.columnSeparator;
         }
-        this.xmlId = properties.id || this.xmlId;
-        this.nodeId = properties.root || null;
+        this.xmlId = (!this.getAttribute('xml-id') && properties.id) || this.xmlId;
+        this.nodeId = (!this.getAttribute('xml-id') && properties.root) || null;
 
         if (properties.path) {
             this.getDocument().path = properties.path;
