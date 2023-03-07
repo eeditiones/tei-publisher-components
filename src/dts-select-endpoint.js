84f789c97b045b129a21d353908168e7a9f1b823
Joern Turner <joern@existsolutions.com>
2023-03-07 10:37:26 +0100

    feat(pb-grid):migrated to Url Registry commit() (replaces pbMixin.pushHistory) removed localStorage as state is fully url-driven now

--- src/dts-select-endpoint.js
+++ src/dts-select-endpoint.js
@@ -4,16 +4,17 @@ import { translate } from "./pb-i18n.js";
 import '@polymer/paper-dropdown-menu/paper-dropdown-menu.js';
 import '@polymer/paper-listbox';
 import '@polymer/paper-item';
+import {registry} from "./urls.js";
 
 /**
  * A dropdown to select a DTS endpoint from a configured list.
  * The list may either be given as a JSON-formatted string within the
  * `endpoints` property or it can be loaded from a JSON file whose path
  * is specified via the `load` property.
- * 
+ *
  * The JSON should contain an array of objects, each having an `url` and
  * `title` property.
- * 
+ *
  * @fires dts-endpoint - Sets the endpoint
  */
 export class DtsSelectEndpoint extends pbMixin(LitElement) {
@@ -54,12 +55,13 @@ export class DtsSelectEndpoint extends pbMixin(LitElement) {
     connectedCallback() {
         super.connectedCallback();
 
-        this.endpoint = this.getParameter('endpoint');
+        // this.endpoint = this.getParameter('endpoint');
+        this.endpoint = registry.get(decodeURI('endpoint'));
     }
 
     updated(changedProperties) {
         super.updated();
-        if (changedProperties.has('endpoints')) {
+        if (changedProperties.has('endpoint')) {
             const item = this.shadowRoot.getElementById('endpoints').selectedItem;
             if (!item && this.auto && this.endpoints.length > 0) {
                 this.endpoint = this.endpoints[0].url;
@@ -96,8 +98,11 @@ export class DtsSelectEndpoint extends pbMixin(LitElement) {
             return;
         }
         const endpoint = this.endpoints.find((endp) => endp.url === newEndpoint);
-        this.setParameter('endpoint', endpoint.url);
-        this.pushHistory('dts-endpoint');
+
+        // this.setParameter('endpoint', endpoint.url);
+        // this.pushHistory('dts-endpoint');
+        registry.replace(this,{ endpoint:encodeURI(endpoint.url)})
+
         console.log('<dts-select-endpoint> Setting endpoint to %s', newEndpoint);
         this.emitTo('dts-endpoint', {
             endpoint: endpoint.url,
