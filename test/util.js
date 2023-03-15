8d77cc906fb363afa1c1150a4dbe959e21f05fbc
Juri Leino <github@line-o.de>
2023-03-16 00:02:01 +0100

    test(util): add cleanup function
    Calls to fixtureCleanup were in all afterEach steps.
    Also reset URL to further isolate testcases with the introduction of
    URL registry.

--- test/util.js
+++ test/util.js
@@ -1,4 +1,11 @@
-import { fixture, waitUntil } from '@open-wc/testing';
+import { fixture, waitUntil, fixtureCleanup } from '@open-wc/testing';
+
+export function cleanup () {
+    fixtureCleanup();
+    // registry might have modified the internal URL
+    // this line will reset it to the initial URL
+    window.history.replaceState(null, '', '/context.html');
+}
 
 export async function waitForPage(template, type = 'pb-page-ready') {
     let initDone;
