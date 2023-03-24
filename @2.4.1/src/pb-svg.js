51fd9d95c58252eac6d3976aacc096336f2d5937
wolfgangmm <wolfgangmm@users.noreply.github.com>
2023-03-24 15:58:14 +0000

    deploy: fe2f0682682172c5b7e3e348dedfe1951ceabb5c

---
+++ @2.4.1/src/pb-svg.js
@@ -0,0 +1,101 @@
+import { LitElement, html, css } from 'lit-element';
+import "@lrnwebcomponents/es-global-bridge";
+import { pbMixin } from './pb-mixin.js';
+
+
+/**
+ * Show an SVG image with zoom and pan functionality. The image URL may
+ * either be specified via the `url` property or an `pb-show-annotation` event
+ * sent to this component.
+ *
+ * @fires pb-show-annotation - When received, loads the image from the URL passed from the event
+ * @cssprop --pb-svg-height - Height of the SVG element
+ * @cssprop --pb-svg-width - Width of the SVG element
+ */
+export class PbSvg extends pbMixin(LitElement) {
+    static get properties() {
+        return {
+            ...super.properties,
+            /**
+             * The URL to load the SVG from.
+             */
+            url: {
+                type: String
+            }
+        };
+    }
+
+    constructor() {
+        super();
+        this._pan = null;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+    }
+
+    firstUpdated() {
+        super.firstUpdated();
+        window.ESGlobalBridge.requestAvailability();
+        window.ESGlobalBridge.instance.load("svg-pan-zoom", `https://cdn.jsdelivr.net/npm/svg-pan-zoom@3.6.1/dist/svg-pan-zoom.min.js`);
+        window.addEventListener(
+            "es-bridge-svg-pan-zoom-loaded",
+            this._onSvgPanZoomLoaded.bind(this),
+            { once: true }
+        );
+
+        this.container = this.shadowRoot.getElementById('image');
+
+        this.subscribeTo('pb-show-annotation', (ev) => {
+            console.log('<pb-svg> loading %s', ev.detail.file);
+            this.url = ev.detail.file;
+            this.load();
+        });
+    }
+
+    _onSvgPanZoomLoaded() {
+        this.load();
+    }
+ 
+    load() {
+        if (this._pan) {
+            this._pan.destroy();
+            this._pan = null;
+            this.container.innerHTML = '';
+        }
+        if (!this.url) {
+            return;
+        }
+        fetch(this.url)
+            .then((response) => response.text())
+            .then((data) => {
+                const doc = new DOMParser().parseFromString(data, "image/svg+xml");
+                const svg = doc.documentElement;
+                this.container.appendChild(svg);
+                this._pan =
+                    window.svgPanZoom(svg, {
+                        controlIconsEnabled: true,
+                        fit: true,
+                        center: true
+                    });
+            });
+    }
+
+    render() {
+        return html`<div id="image"></div>`;
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+            }
+
+            #image svg {
+                height: var(--pb-svg-height, 100%);
+                width: var(--pb-svg-width, 100%);
+            }
+        `;
+    }
+}
+customElements.define('pb-svg', PbSvg);
---
+++ @2.4.1/src/pb-tabs.js
@@ -0,0 +1,79 @@
+import { LitElement, html, css } from 'lit-element';
+import '@polymer/paper-tabs';
+import '@polymer/iron-pages';
+import { pbMixin, waitOnce } from './pb-mixin.js';
+import { registry } from "./urls.js";
+
+
+/**
+ * Combines a row of tabs with associated content.
+ * 
+ * @slot tab - tab area
+ * @slot page - page area
+ * @csspart pages - wrapper around the tab pages
+ */
+export class PbTabs extends pbMixin(LitElement) {
+    static get properties() {
+        return {
+            ...super.properties,
+            selected: {
+                type: Number,
+                reflect: true
+            }
+        };
+    }
+
+    constructor() {
+        super();
+
+        this.selected = 0;
+        this._initialized = false;
+    }
+    
+    connectedCallback() {
+        super.connectedCallback();
+
+        waitOnce('pb-page-ready', () => {
+            this.selected = registry.state.tab || 0;
+
+            registry.subscribe(this, (state) => {
+                this.selected = state.tab;
+            });
+
+        });
+    }
+
+    _switchTab(ev) {
+        const current = parseInt(ev.detail.value, 10);
+        if (this.selected === current) {
+            return;
+        }
+        this.selected = current;
+        if (this._initialized) {
+            registry.commit(this, { tab: this.selected });
+        } else {
+            registry.replace(this, { tab: this.selected });
+        }
+        this._initialized = true;
+    }
+
+    render() {
+        return html`
+            <paper-tabs id="tabs" selected="${this.selected || 0 }" @selected-changed="${this._switchTab}">
+                <slot name="tab"></slot>
+            </paper-tabs>
+            <iron-pages part="pages" selected="${this.selected}">
+                <slot name="page"></slot>
+            </iron-pages>
+        `;
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+            }
+        `;
+    }
+}
+customElements.define('pb-tabs', PbTabs);
