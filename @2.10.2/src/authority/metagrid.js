56139181e80a24d57118e6d59c82cfe02fb20f93
wolfgangmm <wolfgangmm@users.noreply.github.com>
2023-07-22 17:56:33 +0000

    deploy: 9aa22bfc800b968b6490695a7823ccaf85552077

---
+++ @2.10.2/src/authority/metagrid.js
@@ -0,0 +1,85 @@
+/* eslint-disable class-methods-use-this */
+import { Registry } from './registry.js';
+
+export class Metagrid extends Registry {
+
+  async query(key) {
+    const query = key.replace(/[^\w\s]+/g, '');
+    const results = [];
+    const url = `https://api.metagrid.ch/search?query=${encodeURIComponent(query)}`;
+    
+    return new Promise((resolve) => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            json.resources.forEach(item => {
+              const name = `${item.metadata.last_name}, ${item.metadata.first_name} `;
+              const result = {
+                register: this._register,
+                id: `${item.provider.slug}-${item.identifier}`,
+                label: name,
+                details: `${item.metadata.birth_date} - ${item.metadata.death_date}`,
+                link: item.link.uri,
+                provider: item.provider.slug
+              };
+              results.push(result);
+            });
+            resolve({
+              totalItems: json.meta.total,
+              items: results,
+            });
+        });
+    })
+  }
+
+  info(key, container) {
+    const p = key.indexOf('-');
+    const slug = key.substring(0, p);
+    return new Promise((resolve) => {
+      this.getRecord(key)
+      .then(json => {
+        const output = `
+          <h3 class="label">
+            <a href="https://${json.link.uri}" target="_blank">
+              ${json.metadata.last_name}, ${json.metadata.first_name}
+            </a>
+          </h3>
+          <p>${json.metadata.birth_date} - ${json.metadata.death_date}</p>
+        `;
+        container.innerHTML = output;
+        resolve({
+          id: `${slug}-${json.identifier}`,
+          strings: [`${json.metadata.first_name} ${json.metadata.last_name}`]
+        })
+      });
+    });
+  }
+
+  /**
+   * Retrieve a raw JSON record for the given key as returned by the endpoint.
+   * 
+   * @param {string} key the key to look up
+   * @returns {Promise<any>} promise resolving to the JSON record returned by the endpoint
+   */
+   async getRecord(key) {
+    const p = key.indexOf('-');
+    const slug = key.substring(0, p);
+    const id = key.substring(p + 1);
+    return fetch(`https://api.metagrid.ch/search?slug=${slug}&query=${id}`)
+    .then(response => response.json())
+    .then(json => {
+      const item = json.resources[0];
+      const output = Object.assign({}, item);
+      output.name = `${item.metadata.first_name} ${item.metadata.last_name}`;
+      output.links = [`https://${item.link.uri}`];
+      if (item.metadata.birth_date && item.metadata.birth_date.length > 0) {
+        output.birth = item.metadata.birth_date;
+      }
+      if (item.metadata.death_date && item.metadata.death_date.length > 0) {
+        output.death = item.metadata.death_date;
+      }
+      return output;
+    })
+    .catch((reason) => Promise.reject(reason));
+}
+}
---
+++ @2.10.2/src/docs/pb-components-list.js
@@ -0,0 +1,64 @@
+import { LitElement, html, css } from 'lit-element';
+import '@polymer/paper-item';
+
+/**
+ * Displays a list of components to view.
+ * 
+ * @fires pb-api-component if a component name is clicked
+ */
+export class PbComponentsList extends LitElement {
+    static get properties() {
+        return {
+            json: {
+                type: Object
+            },
+            withDemo: {
+                type: Boolean,
+                attribute: 'with-demo'
+            },
+            _demos: {
+                type: Object
+            }
+        };
+    }
+
+    constructor() {
+        super();
+        this.json = null;
+        this.withDemo = false;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+    }
+
+    render() {
+        if (this.json) {
+            const { tags } = this.json;
+            tags.sort((a, b) => a.name.localeCompare(b.name));
+            let elements = tags;
+            if (this.withDemo) {
+                elements = tags.filter((tag) => tag.demo);
+            }
+            return html`
+                ${elements.map((tag) => html`<paper-item @click="${() => PbComponentsList._viewComponent(tag)}">${tag.name}</paper-item>`)}
+            `;
+        }
+        return html`<div>Loading ...</div>`;
+    }
+
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+            }
+        `;
+    }
+
+    static _viewComponent(component, tab = 0) {
+        document.dispatchEvent(new CustomEvent('pb-api-component', {
+            detail: { component, tab }
+        }));
+    }
+}
+customElements.define('pb-components-list', PbComponentsList);
