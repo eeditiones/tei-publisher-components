eeditiones/tei-publisher-components

commit 189d5a1ac87014db3452f846cf506616a12959f4

fix(pb-mixin): the wait method needs to make sure the HTML DOM has been loaded entirely before selecting elements to wait for

src/pb-mixin.js
@@ -217,33 +217,35 @@ export const pbMixin = (superclass) => class PbMixin extends superclass {
             callback();
             return;
         }
-        const targetNodes = Array.from(document.querySelectorAll(this.waitFor));
-        const targets = targetNodes.filter(target => this.emitsOnSameChannel(target));
-        const targetCount = targets.length;
-        if (targetCount === 0) {
-            // selector did not return any targets
-            callback();
-            return;
-        }
-        let count = targetCount;
-        targets.forEach((target) => {
-            if (target._isReady) {
-                count -= 1;
-                if (count === 0) {
-                    callback();
-                }
+        window.addEventListener('DOMContentLoaded', () => {
+            const targetNodes = Array.from(document.querySelectorAll(this.waitFor));
+            const targets = targetNodes.filter(target => this.emitsOnSameChannel(target));
+            const targetCount = targets.length;
+            if (targetCount === 0) {
+                // selector did not return any targets
+                callback();
                 return;
             }
-            const handler = target.addEventListener('pb-ready', (ev) => {
-                if (ev.detail.source === this) {
-                    // same source: ignore
+            let count = targetCount;
+            targets.forEach((target) => {
+                if (target._isReady) {
+                    count -= 1;
+                    if (count === 0) {
+                        callback();
+                    }
                     return;
                 }
-                count -= 1;
-                if (count === 0) {
-                    target.removeEventListener('pb-ready', handler);
-                    callback();
-                }
+                const handler = target.addEventListener('pb-ready', (ev) => {
+                    if (ev.detail.source === this) {
+                        // same source: ignore
+                        return;
+                    }
+                    count -= 1;
+                    if (count === 0) {
+                        target.removeEventListener('pb-ready', handler);
+                        callback();
+                    }
+                });
             });
         });
     }

commit afb665d9a720c484bbf0549628780920f7a91447

fix(pb-mixin): fix bug in wait method introduced by 189d5a1ac87014db3452f846cf506616a12959f4

src/pb-mixin.js
@@ -213,11 +213,7 @@ export const pbMixin = (superclass) => class PbMixin extends superclass {
      * @param {Function} callback function to be called when all components are ready
      */
     wait(callback) {
-        if (!this.waitFor) {
-            callback();
-            return;
-        }
-        window.addEventListener('DOMContentLoaded', () => {
+        const _checkAndWait = () => {
             const targetNodes = Array.from(document.querySelectorAll(this.waitFor));
             const targets = targetNodes.filter(target => this.emitsOnSameChannel(target));
             const targetCount = targets.length;
@@ -247,7 +243,19 @@ export const pbMixin = (superclass) => class PbMixin extends superclass {
                     }
                 });
             });
-        });
+        }
+
+        if (!this.waitFor) {
+            callback();
+            return;
+        }
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', () => {
+                _checkAndWait();
+            });
+        } else {
+            _checkAndWait();
+        }
     }
 
     /**