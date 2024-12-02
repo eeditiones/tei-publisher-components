910700440aaaba3d91729ae8da820ef10a7f69a8
helenasabel <helena.b.sabel@gmail.com>
2024-12-02 14:00:34 +0100

    feat(pb-authority-lookup): add work entity

--- src/authority/gnd.js
+++ src/authority/gnd.js
@@ -41,6 +41,9 @@ export class GND extends Registry {
       case 'organization':
         filter = 'CorporateBody';
         break;
+      case 'work':
+          filter = 'Work';
+          break;
       default:
         filter = 'Person';
         break;
