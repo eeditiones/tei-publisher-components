cddb36fddd35e4e31b6bbd1a98af1f3c9275a92e
wolfgangmm <wolfgangmm@exist-db.org>
2025-06-15 16:34:06 +0200

    feat(pb-zoom): replace paper button

--- src/pb-zoom.js
+++ src/pb-zoom.js
@@ -1,7 +1,7 @@
-import { LitElement, html } from 'lit-element';
+import { LitElement, css, html } from 'lit-element';
 import { pbMixin } from './pb-mixin';
-import '@polymer/iron-icons';
-import '@polymer/paper-icon-button';
+import { translate } from './pb-i18n';
+import { themableMixin } from './theming';
 
 /**
  * Zoom button to enlarge/shrink the font for the views. This component does not
@@ -9,7 +9,7 @@ import '@polymer/paper-icon-button';
  *
  * @fires pb-zoom - sends an event for e.g. pb-views to react to
  */
-export class PbZoom extends pbMixin(LitElement) {
+export class PbZoom extends themableMixin(pbMixin(LitElement)) {
     static get properties() {
         return {
             ...super.properties,
@@ -44,7 +44,27 @@ export class PbZoom extends pbMixin(LitElement) {
 
     render() {
         return html`
-            <paper-icon-button icon="${this.icon}" @click="${this._handleClick}"></paper-icon-button>
+            <a href="#" @click="${this._handleClick}" title="${this.direction === 'in' ? translate('toolbar.zoom.in') : translate('toolbar.zoom.out')}">
+                <slot name="icon">
+                    ${this.direction === 'in' ? html`
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748ZM10 10V7H12V10H15V12H12V15H10V12H7V10H10Z"></path></svg>
+                    ` : html`
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748ZM7 10H15V12H7V10Z"></path></svg>
+                    `}
+                </slot>
+            </a>
+        `;
+    }
+
+    static get styles() {
+        return css`
+            a {
+                color: inherit;
+                text-decoration: none;
+            }
+            a:hover {
+                color: inherit;
+            }
         `;
     }
 
