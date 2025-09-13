8618eda0259580954aea62891c9206637aed7db0
duncdrum <d.paterson@me.com>
2025-09-13 20:27:16 +0200

    fix(components): upgrade wave 2

--- src/pb-tabs.js
+++ src/pb-tabs.js
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html, css } from 'lit';
 import '@polymer/paper-tabs';
 import '@polymer/iron-pages';
 import { pbMixin, waitOnce } from './pb-mixin.js';
