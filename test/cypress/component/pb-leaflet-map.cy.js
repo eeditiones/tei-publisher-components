9833c3a747373eb936ef4434bd6da8cec7bf6094
duncdrum <d.paterson@me.com>
2025-09-13 21:32:37 +0200

    chore(*.cy.js): add placeholders

---
+++ test/cypress/component/pb-leaflet-map.cy.js
@@ -0,0 +1,4 @@
+describe('pb-leaflet-map (placeholder)', () => {
+  it.skip('covered in e2e (assets, map libs)', () => {})
+})
+
---
+++ test/cypress/component/pb-tify.cy.js
@@ -0,0 +1,4 @@
+describe('pb-tify (placeholder)', () => {
+  it.skip('covered in e2e (viewer deps/assets)', () => {})
+})
+
