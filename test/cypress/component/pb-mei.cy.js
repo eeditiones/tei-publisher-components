6fc90178f7bfcbe0dd261408d1d040761890c7d0
duncdrum <d.paterson@me.com>
2025-09-13 21:32:37 +0200

    chore(*.cy.js): add placeholders

---
+++ test/cypress/component/pb-mei.cy.js
@@ -0,0 +1,4 @@
+describe('pb-mei (placeholder)', () => {
+  it.skip('covered in e2e (wasm/assets)', () => {})
+})
+
---
+++ test/cypress/component/pb-odd-editor.cy.js
@@ -0,0 +1,4 @@
+describe('pb-odd-editor (placeholder)', () => {
+  it.skip('covered in e2e (complex editors)', () => {})
+})
+
