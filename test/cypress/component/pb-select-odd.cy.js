018baa9b88d8b29f780c6fe61bf543b84052a3fc
duncdrum <d.paterson@me.com>
2025-09-13 20:55:45 +0200

    test(select-odd, select-template): add tests

---
+++ test/cypress/component/pb-select-odd.cy.js
@@ -0,0 +1,15 @@
+import '../../../src/pb-page.js'
+import '../../../src/pb-select-odd.js'
+
+describe('pb-select-odd', () => {
+  it('mounts and renders dropdown', () => {
+    const html = '<pb-page endpoint="." api-version="1.0.0"><pb-select-odd id="sel" odd="default"></pb-select-odd></pb-page>'
+    cy.mount(html)
+    cy.document().then((doc) => {
+      doc.dispatchEvent(new CustomEvent('pb-page-ready', { detail: { endpoint: '.', apiVersion: '1.0.0' } }))
+    })
+    cy.get('#sel').should('exist')
+    cy.get('#sel').find('paper-dropdown-menu').should('exist')
+  })
+})
+
