63710c79491355ffda77a3f1d4dd921203e5c9a9
duncdrum <d.paterson@me.com>
2025-09-13 20:27:16 +0200

    fix(components): upgrade wave 2

--- src/pb-link.js
+++ src/pb-link.js
@@ -1,5 +1,5 @@
-import { LitElement, html } from 'lit-element';
-import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
+import { LitElement, html } from 'lit';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import { pbMixin } from './pb-mixin.js';
 import { registry } from './urls.js';
 
