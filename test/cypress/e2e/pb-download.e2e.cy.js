4e2f3236f1d44044814235dd84e114737e792bb2
duncdrum <d.paterson@me.com>
2025-09-14 13:48:33 +0200

    test(download.e2e): add test

---
+++ test/cypress/e2e/pb-download.e2e.cy.js
@@ -0,0 +1,19 @@
+// E2E: pb-download computes href and target
+
+describe('Demo: pb-download', () => {
+  beforeEach(() => {
+    cy.visit('/demo/pb-download.html')
+  })
+
+  it('computes href for document-based download', () => {
+    cy.get('pb-download[type="pdf"][src="document1"]').find('#button')
+      .should('have.attr', 'href')
+      .and('match', /api\/document\/.*\/pdf\?odd=.*\.odd&token=/)
+      .and('include', 'docbook.odd')
+  })
+
+  it('sets target _blank when source flag is present', () => {
+    cy.contains('paper-button', 'FO source').parents('pb-download').find('#button')
+      .should('have.attr', 'target', '_blank')
+  })
+})
