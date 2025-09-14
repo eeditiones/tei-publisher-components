d32cc6bd3d20909e5f6d4a73aa2e198da22f70c9
duncdrum <d.paterson@me.com>
2025-09-14 11:13:09 +0200

    fix(demo/grid): avoid dual imports
    add test

---
+++ test/cypress/e2e/pb-grid.e2e.cy.js
@@ -0,0 +1,26 @@
+// E2E: pb-grid demo basic interactions (add/remove panels)
+
+describe('Demo: pb-grid', () => {
+  beforeEach(() => {
+    cy.visit('/demo/pb-grid.html')
+  })
+
+  it('adds and removes panels', () => {
+    // Initial panels rendered from panels="[0,1]"
+    cy.get('#grid ._grid_panel').then(($panels) => {
+      const initial = $panels.length
+      expect(initial).to.be.greaterThan(0)
+    })
+
+    // Click the Add Column action
+    cy.get('pb-grid-action.grid-add').find('a').click({ force: true })
+    cy.get('#grid ._grid_panel').should('have.length.greaterThan', 2)
+
+    // Remove the first panel via its toolbar action
+    cy.get('#grid ._grid_panel').first().within(() => {
+      cy.get('pb-grid-action').find('a').click({ force: true })
+    })
+    cy.get('#grid ._grid_panel').should('have.length.greaterThan', 1)
+  })
+})
+
