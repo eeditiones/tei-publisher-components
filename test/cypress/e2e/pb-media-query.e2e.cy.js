746672fe07cbbac4d1878480fc47e7c0fe5e8d7b
duncdrum <d.paterson@me.com>
2025-09-14 16:03:13 +0200

    chore(e2e): add scaffolds

---
+++ test/cypress/e2e/pb-media-query.e2e.cy.js
@@ -0,0 +1,10 @@
+// E2E: pb-media-query scaffolded test
+describe('Demo: pb-media-query', () => {
+  beforeEach(() => {
+    cy.visit('/demo/pb-media-query.html')
+  })
+
+  it.skip('has a scaffolded e2e test', () => {
+    // TODO: add meaningful assertions once pb-media-query migration is complete
+  })
+})
---
+++ test/cypress/e2e/pb-zoom.e2e.cy.js
@@ -0,0 +1,10 @@
+// E2E: pb-zoom scaffolded test
+describe('Demo: pb-zoom', () => {
+  beforeEach(() => {
+    cy.visit('/demo/pb-zoom.html')
+  })
+
+  it.skip('has a scaffolded e2e test', () => {
+    // TODO: add meaningful assertions once pb-zoom migration is complete
+  })
+})
