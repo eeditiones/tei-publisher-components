32819ad1fe664f3fdfd8b25c15f5f9d4c9684ac7
duncdrum <d.paterson@me.com>
2025-09-16 16:48:09 +0200

    feat(pb-document, pb-i18n, pb-view, pb-drawer, pb-highlight): enhance event handling, improve test coverage, and remove scaffolded tests

--- test/cypress/e2e/pb-document.e2e.cy.js
+++ test/cypress/e2e/pb-document.e2e.cy.js
@@ -1,10 +1,18 @@
-// E2E: pb-document scaffolded test
+// E2E: pb-document demo
 describe('Demo: pb-document', () => {
   beforeEach(() => {
     cy.visit('/demo/pb-document.html')
   })
 
-  it.skip('has a scaffolded e2e test', () => {
-    // TODO: add meaningful assertions once pb-document migration is complete
+  it('renders the demo with two pb-document elements', () => {
+    cy.get('pb-document').should('have.length', 2)
+    cy.get('#document1').should('have.attr', 'path').and('include', 'kant_rvernunft')
+    cy.get('#document2').should('have.attr', 'path').and('include', 'documentation.xml')
   })
+
+  it('is referenced by pb-view elements', () => {
+    cy.get('pb-view#view1').should('have.attr', 'src', 'document1')
+    cy.get('pb-view#view2').should('have.attr', 'src', 'document2')
+  })
+
 })
--- test/cypress/e2e/pb-drawer.e2e.cy.js
+++ test/cypress/e2e/pb-drawer.e2e.cy.js
@@ -1,10 +1,30 @@
-// E2E: pb-drawer scaffolded test
+// E2E: pb-drawer
+// StandardJS formatting
+
 describe('Demo: pb-drawer', () => {
   beforeEach(() => {
     cy.visit('/demo/pb-drawer.html')
   })
 
-  it.skip('has a scaffolded e2e test', () => {
-    // TODO: add meaningful assertions once pb-drawer migration is complete
+  it('renders and is closed by default', () => {
+    cy.get('pb-drawer').should('exist').and(($el) => {
+      expect($el[0].hasAttribute('opened'), 'no opened attribute').to.be.false
+    })
+  })
+
+  it('can be opened and closed programmatically via attribute', () => {
+    // open
+    cy.get('pb-drawer').then(($el) => {
+      const el = $el[0]
+      el.setAttribute('opened', '')
+    })
+    cy.get('pb-drawer').should('have.attr', 'opened')
+
+    // close
+    cy.get('pb-drawer').then(($el) => {
+      const el = $el[0]
+      el.removeAttribute('opened')
+    })
+    cy.get('pb-drawer').should('not.have.attr', 'opened')
   })
 })
