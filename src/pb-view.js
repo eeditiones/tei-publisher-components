f543e9b03696092627dce0ac61e8f3ca4b7cc1ae
duncdrum <d.paterson@me.com>
2025-09-16 16:48:09 +0200

    feat(pb-document, pb-i18n, pb-view, pb-drawer, pb-highlight): enhance event handling, improve test coverage, and remove scaffolded tests

--- src/pb-view.js
+++ src/pb-view.js
@@ -354,6 +354,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
     this._chunks = [];
     this._scrollTarget = null;
     this.static = null;
+    this._lastRequestKey = null;
   }
 
   attributeChangedCallback(name, oldVal, newVal) {
@@ -658,6 +659,16 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
   }
 
   _doLoad(params) {
+    // De-duplicate identical requests to avoid hammering the backend
+    const docPath = this.getDocument && this.getDocument() ? this.getDocument().path : ''
+    const reqKey = JSON.stringify({ url: this.url || '', doc: docPath, params })
+    if (this._lastRequestKey === reqKey) {
+      // nothing changed; unlock and skip
+      this._loading = false
+      return
+    }
+    this._lastRequestKey = reqKey
+
     this.emitTo('pb-start-update', params);
 
     console.log('<pb-view> Loading view with params %o', params);
@@ -748,6 +759,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
 
   _handleError() {
     this._clear();
+    this._loading = false;
     const loader = this.shadowRoot.getElementById('loadContent');
     let message;
     const { response } = loader.lastError;
@@ -774,6 +786,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
     const resp = loader.lastResponse;
 
     if (!resp) {
+      this._loading = false;
       console.error('<pb-view> No response received');
       return;
     }
@@ -782,6 +795,7 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
         this._content = this.notFound;
       }
       this.emitTo('pb-end-update', null);
+      this._loading = false;
       return;
     }
 
@@ -830,6 +844,8 @@ export class PbView extends themableMixin(pbMixin(LitElement)) {
     });
 
     this.emitTo('pb-end-update', null);
+    // allow subsequent loads with new params
+    this._loading = false;
   }
 
   _replaceContent(resp, direction) {
--- test/cypress/e2e/pb-highlight.e2e.cy.js
+++ test/cypress/e2e/pb-highlight.e2e.cy.js
@@ -1,10 +1,19 @@
-// E2E: pb-highlight scaffolded test
+// E2E: pb-highlight tests
 describe('Demo: pb-highlight', () => {
   beforeEach(() => {
+    // do not log css transform requests
+    cy.intercept(
+      { method: 'GET', url: '**/components.css', middleware: true },
+      (req) => { req.continue() }
+      , { log: false }).as('demoCss')
+
     cy.visit('/demo/pb-highlight.html')
   })
 
-  it.skip('has a scaffolded e2e test', () => {
-    // TODO: add meaningful assertions once pb-highlight migration is complete
+  it('loads and renders text inside pb-highlight', () => {
+    cy.get('pb-highlight')
+      .should('exist')
+      .invoke('text')
+      .should('not.be.empty')
   })
 })
