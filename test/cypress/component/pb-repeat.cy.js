f28c2836a17eb7da45ad061cfaf1c1eea54830b1
duncdrum <d.paterson@me.com>
2025-09-29 15:54:18 +0200

    refactor: replace polymer buttons
    wip

--- test/cypress/component/pb-repeat.cy.js
+++ test/cypress/component/pb-repeat.cy.js
@@ -10,5 +10,15 @@ describe('pb-repeat', () => {
   it('renders initial instance', () => {
     cy.get('#rep .instances .instance').should('have.length', 1)
   })
-})
 
+  it('uses native icon buttons instead of paper-icon-button', () => {
+    cy.get('#rep').find('paper-icon-button').should('not.exist')
+    cy.get('#rep')
+      .find('button.pb-button--icon')
+      .should('have.length', 2)
+      .each($btn => {
+        expect($btn).to.have.attr('type', 'button')
+        expect($btn).to.have.attr('aria-label')
+      })
+  })
+})
--- test/cypress/e2e/pb-clipboard.e2e.cy.js
+++ test/cypress/e2e/pb-clipboard.e2e.cy.js
@@ -10,8 +10,45 @@ describe('Demo: pb-clipboard', () => {
       }
       win.navigator.clipboard.writeText = cy.stub().as('writeText')
     })
-    cy.get('pb-clipboard').first().find('paper-icon-button').click({ force: true })
+    cy.get('pb-clipboard')
+      .first()
+      .find('button.pb-button--icon')
+      .as('copyButton')
+
+    cy.get('@copyButton').should('have.attr', 'aria-label')
+    cy.get('@copyButton').click({ force: true })
     cy.get('@writeText').should('have.been.called')
     cy.get('@writeText').its('firstCall.args.0').should('match', /john doe/i)
   })
+
+  it.skip('should meet accessibility standards', () => {
+        // Check for proper ARIA attributes
+        cy.get('pb-clipboard')
+          .find('button, input, select, textarea')
+          .each($el => {
+            // Buttons should have type attribute
+            if ($el[0].tagName === 'BUTTON') {
+              cy.wrap($el).should('have.attr', 'type')
+            }
+            // Interactive elements should be focusable
+            cy.wrap($el).should('not.have.attr', 'tabindex', '-1')
+          })
+        // Check focus management
+        cy.get('pb-clipboard')
+          .find('button:first')
+          .focus()
+          .should('be.focused')
+      })
+
+  it.skip('should support keyboard navigation', () => {
+        // Test Tab navigation
+        cy.get('body')
+        cy.press("Tab")
+        cy.focused().should('exist')
+        // Test Enter/Space on buttons
+        cy.get('pb-clipboard')
+          .find('button:first')
+          .focus()
+          .type('{enter}')
+      })
 })
