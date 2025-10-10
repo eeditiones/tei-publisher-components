c075145b7ccd4d6e7ea80f2342cf67ea08cf25d3
duncdrum <d.paterson@me.com>
2025-10-10 11:06:05 +0200

    test(component): pb-split-list, pb-svg, pb-table-column
    increase coverage

--- test/cypress/component/pb-split-list.cy.js
+++ test/cypress/component/pb-split-list.cy.js
@@ -6,5 +6,99 @@ describe('pb-split-list', () => {
     cy.mount('<pb-split-list></pb-split-list>')
     cy.get('pb-split-list').should('exist')
   })
-})
 
+  it('accepts url attribute', () => {
+    cy.mount('<pb-split-list url="api/people"></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].url).to.equal('api/people')
+    })
+  })
+
+  it('accepts selected attribute', () => {
+    cy.mount('<pb-split-list selected="A"></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].selected).to.equal('A')
+    })
+  })
+
+  it('accepts subforms attribute', () => {
+    cy.mount('<pb-split-list subforms="#myform"></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].subforms).to.equal('#myform')
+    })
+  })
+
+  it('has load method', () => {
+    cy.mount('<pb-split-list></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].load).to.be.a('function')
+    })
+  })
+
+  it('has submit method', () => {
+    cy.mount('<pb-split-list></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].submit).to.be.a('function')
+    })
+  })
+
+  it('renders header for categories', () => {
+    cy.mount('<pb-split-list></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      const component = $el[0]
+      component._categories = [
+        { category: 'A', count: 3 },
+        { category: 'B', count: 2 }
+      ]
+      component.selected = 'A'
+      component.requestUpdate()
+      return component.updateComplete
+    })
+    
+    cy.get('pb-split-list').find('header').should('exist')
+    cy.get('pb-split-list').find('[part*="category"]').should('have.length', 2)
+  })
+
+  it('renders active category with correct part', () => {
+    cy.mount('<pb-split-list></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      const component = $el[0]
+      component._categories = [
+        { category: 'A', count: 3 },
+        { category: 'B', count: 2 }
+      ]
+      component.selected = 'A'
+      component.requestUpdate()
+      return component.updateComplete
+    })
+    
+    cy.get('pb-split-list').find('[part="active-category"]').should('exist')
+    cy.get('pb-split-list').find('[part="active-category"]').should('contain.text', 'A')
+  })
+
+  it('renders slot for items', () => {
+    cy.mount('<pb-split-list><div class="test-item">Test</div></pb-split-list>')
+    cy.get('pb-split-list').find('#items').should('exist')
+    cy.get('pb-split-list').find('.test-item').should('contain.text', 'Test')
+  })
+
+  it('category click changes selection', () => {
+    cy.mount('<pb-split-list></pb-split-list>')
+    cy.get('pb-split-list').then($el => {
+      const component = $el[0]
+      component._categories = [
+        { category: 'A', count: 3 },
+        { category: 'B', count: 2 }
+      ]
+      component.selected = 'A'
+      component._endpoint = '.'
+      component.requestUpdate()
+      return component.updateComplete
+    })
+    
+    cy.get('pb-split-list').find('[part="category"]').contains('B').click()
+    cy.get('pb-split-list').then($el => {
+      expect($el[0].selected).to.equal('B')
+    })
+  })
+})
--- test/cypress/component/pb-svg.cy.js
+++ test/cypress/component/pb-svg.cy.js
@@ -2,9 +2,61 @@
 import '../../../src/pb-svg.js'
 
 describe('pb-svg', () => {
+  beforeEach(() => {
+    // Stub the external svg-pan-zoom library
+    cy.window().then(win => {
+      if (!win.svgPanZoom) {
+        win.svgPanZoom = cy.stub().returns({
+          destroy: cy.stub(),
+          zoom: cy.stub(),
+          pan: cy.stub(),
+          resize: cy.stub(),
+          fit: cy.stub(),
+          center: cy.stub()
+        })
+      }
+    })
+  })
+
   it('mounts', () => {
     cy.mount('<pb-svg></pb-svg>')
     cy.get('pb-svg').should('exist')
   })
-})
 
+  it('accepts url property', () => {
+    cy.mount('<pb-svg url="test.svg"></pb-svg>')
+    cy.get('pb-svg').then($el => {
+      expect($el[0].url).to.equal('test.svg')
+    })
+  })
+
+  it('has load method', () => {
+    cy.mount('<pb-svg></pb-svg>')
+    cy.get('pb-svg').then($el => {
+      expect($el[0].load).to.be.a('function')
+    })
+  })
+
+  it('renders image container', () => {
+    cy.mount('<pb-svg></pb-svg>')
+    cy.get('pb-svg').find('#image').should('exist')
+  })
+
+  it('renders slot content', () => {
+    cy.mount('<pb-svg><div class="test-content">Test</div></pb-svg>')
+    cy.get('pb-svg').find('.test-content').should('contain.text', 'Test')
+  })
+
+  it('url property is reactive', () => {
+    cy.mount('<pb-svg url="test1.svg"></pb-svg>')
+    
+    cy.get('pb-svg').then($el => {
+      $el[0].url = 'test2.svg'
+      return $el[0].updateComplete
+    })
+
+    cy.get('pb-svg').then($el => {
+      expect($el[0].url).to.equal('test2.svg')
+    })
+  })
+})
