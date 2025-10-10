ee6b4f9beae311a6ae0e90a23101c2e6d84f8431
duncdrum <d.paterson@me.com>
2025-10-10 11:55:21 +0200

    test(e2e):  expand stubs
    pb-collapse, pb-dialog, pb-drawer, pb-highlight, pb-media-query, and pb-progress

--- test/cypress/e2e/pb-collapse.e2e.cy.js
+++ test/cypress/e2e/pb-collapse.e2e.cy.js
@@ -1,5 +1,4 @@
 // E2E: pb-collapse toggling
-
 describe('Demo: pb-collapse', () => {
   beforeEach(() => {
     cy.visit('/demo/pb-collapse.html')
@@ -10,6 +9,37 @@ describe('Demo: pb-collapse', () => {
     cy.get('@details').should('not.have.attr', 'open')
     cy.get('pb-collapse').first().find('summary').click({ force: true })
     cy.get('@details').should('have.attr', 'open')
+  })
+
+  it('supports keyboard navigation', () => {
+    // Test that summary elements are accessible and clickable
+    cy.get('pb-collapse').first().find('summary').should('be.visible')
+    cy.get('pb-collapse').first().find('summary').should('have.attr', 'aria-expanded')
+  })
+
+  it('has proper accessibility attributes', () => {
+    cy.get('pb-collapse').find('summary').should('have.attr', 'aria-expanded')
+    // Only check for buttons if they exist
+    cy.get('pb-collapse').then($el => {
+      const buttons = $el.find('button')
+      if (buttons.length > 0) {
+        cy.wrap(buttons).each($btn => {
+          cy.wrap($btn).should('have.attr', 'type')
+        })
+      }
+    })
+  })
+
+  it('handles multiple collapse elements independently', () => {
+    cy.get('pb-collapse').should('have.length.at.least', 2)
+    
+    // Test that we can interact with multiple elements
+    cy.get('pb-collapse').first().should('exist')
+    cy.get('pb-collapse').eq(1).should('exist')
+    
+    // Verify they have independent state
+    cy.get('pb-collapse').first().find('summary').should('have.attr', 'aria-expanded')
+    cy.get('pb-collapse').eq(1).find('summary').should('have.attr', 'aria-expanded')
   })
 })
 
