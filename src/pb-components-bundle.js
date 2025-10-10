2ae4092299f727fae3c244eff9ec28d0a0d8830b
duncdrum <d.paterson@me.com>
2025-10-10 10:09:10 +0200

    refactor: Polymer  purge
    - Eliminated @polymer/app-layout, @polymer/iron-label, and @polymer/iron-pages
    - Updated pb-components-bundle.js to remove imports related to app-layout.

--- src/pb-components-bundle.js
+++ src/pb-components-bundle.js
@@ -1,5 +1,3 @@
 import './polymer-hack.js';
-import '@polymer/app-layout/app-scroll-effects/app-scroll-effects.js';
-import '@polymer/app-layout/app-scroll-effects/effects/material.js';
 import './docs/pb-demo-snippet.js';
 import './pb-components.js';
